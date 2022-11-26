959182d898eccbe24223ab4622dc1892ea30df24
bilal-webdeveloper <dallali-bilal@hotmail.fr>
2022-11-26 19:00:59 +0100

    Initial Commit

---
+++ FinanceTracker/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  FinanceTracker
+//
+//  Created by Bilal Dallali on 26/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
