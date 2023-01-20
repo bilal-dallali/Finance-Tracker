01564fd0b0f7c40430982648e82eacd5bffa88f7
bilal-webdeveloper <dallali-bilal@hotmail.fr>
2023-01-20 21:11:39 +0100

    animation toggle currencies

--- FinanceTracker/CurrencySelector.swift
+++ FinanceTracker/CurrencySelector.swift
@@ -14,10 +14,20 @@ struct CurrencySelector: View {
     @State var isSelectedMode = false
     
     var body: some View {
-        HStack {
+        ZStack {
             ForEach(currencies.indices) { index in
                 Image(systemName: selectedCurrency == currencies[index] ? currencies[index].filledIconName : currencies[index].iconName)
                     .font(.system(size: 32))
+                    .offset(x: isSelectedMode ? -CGFloat(index) * 40 : 0)
+                    .opacity(selectedCurrency == currencies[index] || isSelectedMode ? 1 : 0)
+                    .onTapGesture {
+                        if isSelectedMode {
+                            selectedCurrency = currencies[index]
+                        }
+                        withAnimation(.easeInOut(duration: 0.2)) {
+                            isSelectedMode.toggle()
+                        }
+                    }
             }
         }
     }
